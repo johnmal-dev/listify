405cc55f1dfeb189af4dd23f21ee2b4601724590
John Malapit <johncmalapit@gmail.com>
2022-09-05 15:19:45 -0400

    merge conflicts

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
 
-const {cronJob} = require('./email');
+const { cronJob } = require('./email')
 
-cronJob(); // start all cronjobs
+cronJob() // start all cronjobs
 
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({ path: './config/.env' })
 
 // Passport config
 require('./config/passport')(passport)
@@ -28,52 +28,45 @@ app.use(express.json())
 app.use(logger('dev'))
 // Sessions
 app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    })
-  )
-  
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+)
+
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(flash())
-  
+
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
 
-
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-<<<<<<< HEAD
+app.listen(process.env.PORT, () => {
+  console.log('Server is running, you better catch it!')
 })
-
 
 const User = require('./models/User')
 // get all things
 app.get('/getallemails', async (req, res) => {
-  const allEmails = await getAllEmails();
-  if(allEmails) {
-    res.send(allEmails);
-  }
-  else {
-    res.status(503).send("An internal server error occured.")
+  const allEmails = await getAllEmails()
+  if (allEmails) {
+    res.send(allEmails)
+  } else {
+    res.status(503).send('An internal server error occured.')
   }
 })
 
 async function getAllEmails() {
   try {
-    const allUsers = await User.find({}, {email: 1, _id: 1})
+    const allUsers = await User.find({}, { email: 1, _id: 1 })
     return allUsers
-  } catch(err) {
-    console.log("An error occurred: ");
-    console.log(err);
-    return null;
+  } catch (err) {
+    console.log('An error occurred: ')
+    console.log(err)
+    return null
   }
 }
-=======
-})
->>>>>>> jasminesbranch
